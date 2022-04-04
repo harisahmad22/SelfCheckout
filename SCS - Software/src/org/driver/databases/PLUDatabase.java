6bd4438859d30ee6b00f3344b704c7385de1381a
Shufan Zhai <97639328+Zhai790@users.noreply.github.com>
2022-04-04 13:57:08 -0700

    Added PLU Database

--- SCS - Software/src/org/driver/databases/PLUDatabase.java
+++ SCS - Software/src/org/driver/databases/PLUDatabase.java
@@ -1,5 +1,7 @@
 package org.driver.databases;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Map;
 
 import org.lsmr.selfcheckout.PriceLookupCode;
@@ -9,17 +11,43 @@ import org.lsmr.selfcheckout.products.PLUCodedProduct;
 public class PLUDatabase {
 	private Map<PriceLookupCode, PLUCodedProduct> database;
 
-	public PLUDatabase()
-	{
+	public PLUDatabase() {
 		this.database = ProductDatabases.PLU_PRODUCT_DATABASE;
-		
-		//Populate the database with some products and their PLU codes
+
+		// Populate the database with some products and their PLU codes
 //		database.put(null, null)
 	}
-	
-	public Map<PriceLookupCode, PLUCodedProduct> getDatabase()
-	{
+
+	public PLUDatabase(ArrayList<PLUCodedProduct> objects) {
+		this.database = ProductDatabases.PLU_PRODUCT_DATABASE;
+
+		// Populate the database with some products and their PLU codes
+		if (!objects.isEmpty() || objects != null) {
+			for (PLUCodedProduct p : objects) {
+				database.put(p.getPLUCode(), p);
+			}
+		}
+
+	}
+
+	public Map<PriceLookupCode, PLUCodedProduct> getDatabase() {
 		return database;
 	}
-	
+
+	public void addPLUProductToDatabase(PriceLookupCode pluCode, String description, BigDecimal price) {
+		PLUCodedProduct product = new PLUCodedProduct(pluCode, description, price);
+		database.put(pluCode, product);
+	}
+
+	public void addPLUProductToDatabase(PriceLookupCode pluCode, String description, double price) {
+		// Convert to a string first to get exact value after BigDecimal conversion
+		BigDecimal thisPrice = new BigDecimal(Double.toString(price));
+		PLUCodedProduct product = new PLUCodedProduct(pluCode, description, thisPrice);
+		database.put(pluCode, product);
+	}
+
+	public void addPLUProductToDatabase(PLUCodedProduct product) {
+		database.put(product.getPLUCode(), product);
+	}
+
 }
