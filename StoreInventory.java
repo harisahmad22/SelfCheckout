083c507011e8bfc0b371942b34f9902e97b53289
Shufan Zhai <97639328+Zhai790@users.noreply.github.com>
2022-04-06 21:11:13 -0700

    Updated StoreInventory

--- StoreInventory.java
+++ StoreInventory.java
@@ -2,26 +2,70 @@ package org.driver.databases;
 
 import java.util.Map;
 
-import org.lsmr.selfcheckout.PriceLookupCode;
+import org.controlSoftware.data.NegativeNumberException;
 import org.lsmr.selfcheckout.external.ProductDatabases;
+import org.lsmr.selfcheckout.products.BarcodedProduct;
 import org.lsmr.selfcheckout.products.PLUCodedProduct;
 import org.lsmr.selfcheckout.products.Product;
 
 public class StoreInventory {
-	
+
 	private Map<Product, Integer> database;
 
-	public StoreInventory()
-	{
+	public StoreInventory() {
 		this.database = ProductDatabases.INVENTORY;
-		
-		//Populate the database with some products and their PLU codes
-//		database.put(null, null)
 	}
-	
-	public Map<Product, Integer> getDatabase()
-	{
+
+	// quantity of goods is set to same amount for all products. Need to update
+	public StoreInventory(PLUTestProducts PLUInventory, int PLUQuantity, TestProducts barcodeInventory,
+			int barcodeQuantity) {
+
+		this.database = ProductDatabases.INVENTORY;
+
+		if (!PLUInventory.getPLUProductList().isEmpty() || PLUInventory.getPLUProductList() != null) {
+			for (PLUCodedProduct p : PLUInventory.getPLUProductList()) {
+				database.put(p, PLUQuantity);
+			}
+		}
+
+		if (!barcodeInventory.getBarcodedProductList().isEmpty() || barcodeInventory.getBarcodedProductList() != null) {
+			for (BarcodedProduct p : barcodeInventory.getBarcodedProductList()) {
+				database.put(p, barcodeQuantity);
+			}
+		}
+	}
+
+	public Map<Product, Integer> getDatabase() {
 		return database;
 	}
-	
+
+	public void addPLUProductsToInventory(PLUCodedProduct p, int quantity) {
+		database.put(p, quantity);
+	}
+
+	public void addBarcodeProductsToInventory(BarcodedProduct p, int quantity) {
+		database.put(p, quantity);
+	}
+
+	public void updatePLUProductQuantity(PLUCodedProduct p, int change) {
+		Integer oldQuantity = database.get(p);
+
+		if ((oldQuantity - change) >= 0) {
+			Integer updatedQuantity = oldQuantity - change;
+			database.put(p, updatedQuantity);
+		} else {
+			throw new NegativeNumberException();
+		}
+	}
+
+	public void updateBarcodeProductsQuantity(BarcodedProduct p, int change) {
+		Integer oldQuantity = database.get(p);
+
+		if ((oldQuantity - change) >= 0) {
+			Integer updatedQuantity = oldQuantity - change;
+			database.put(p, updatedQuantity);
+		} else {
+			throw new NegativeNumberException();
+		}
+	}
 }
