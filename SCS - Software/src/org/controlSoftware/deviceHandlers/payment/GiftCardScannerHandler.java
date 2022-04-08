4ecfb838e4788d68a6ebd9d9d5488d91ed5ebda4
Dranas786 <59370510+Dranas786@users.noreply.github.com>
2022-04-07 19:58:52 -0600

    latest

--- SCS - Software/src/org/controlSoftware/deviceHandlers/payment/GiftCardScannerHandler.java
+++ SCS - Software/src/org/controlSoftware/deviceHandlers/payment/GiftCardScannerHandler.java
@@ -1,6 +1,17 @@
 package org.controlSoftware.deviceHandlers.payment;
 
+import java.math.BigDecimal;
 import java.util.HashMap;
+import java.util.Map;
+
+import org.controlSoftware.data.GiftCardInfo;
+import org.driver.SelfCheckoutData;
+import org.driver.databases.GiftCardDatabase;
+import org.lsmr.selfcheckout.Card.CardData;
+import org.lsmr.selfcheckout.devices.AbstractDevice;
+import org.lsmr.selfcheckout.devices.CardReader;
+import org.lsmr.selfcheckout.devices.observers.AbstractDeviceObserver;
+import org.lsmr.selfcheckout.devices.observers.CardReaderObserver;
 
 /**
  * @author Divyansh Rana - 30117089
@@ -16,24 +27,106 @@ import java.util.HashMap;
  * 2) Add giftCardDatabase to selfCheckoutData
  */
 
-public class GiftCardScannerHandler 
+public class GiftCardScannerHandler implements CardReaderObserver
 {
-    private String GiftCardNumber;
-    private double value;
+    private SelfCheckoutData stationData;
+	
+	public GiftCardScannerHandler(SelfCheckoutData stationData) {
+		this.stationData = stationData;
+	}
 
-    public double payWithGiftCard(String GiftCardNumber, double valuePurchase)
+/**
+	 * Announces that the indicated device has been enabled.
+	 * 
+	 * @param device
+	 *                 The device that has been enabled.
+	 */
+	@Override
+	public void enabled(AbstractDevice<? extends AbstractDeviceObserver> device){}
+
+	/**
+	 * Announces that the indicated device has been disabled.
+	 * 
+	 * @param device
+	 *                 The device that has been enabled.
+	 */
+	@Override
+	public void disabled(AbstractDevice<? extends AbstractDeviceObserver> device) {}
+	
+	/**
+	 * Announces that a card has been inserted in the indicated card reader.
+	 * 
+	 * @param reader
+	 *            The reader where the event occurred.
+	 */
+	@Override
+	public void cardInserted(CardReader reader) {
+	}
+
+	/**
+	 * Announces that a card has been removed from the indicated card reader.
+	 * 
+	 * @param reader
+	 *            The reader where the event occurred.
+	 */
+	@Override
+	public void cardRemoved(CardReader reader) {
+	}
+
+	/**
+	 * Announces that a (tap-enabled) card has been tapped on the indicated card
+	 * reader.
+	 * 
+	 * @param reader
+	 *            The reader where the event occurred.
+	 */
+	@Override
+	public void cardTapped(CardReader reader) {
+	}
+	
+	/**
+	 * Announces that a card has swiped on the indicated card reader.
+	 * 
+	 * @param reader
+	 *            The reader where the event occurred.
+	 */
+	@Override
+	public void cardSwiped(CardReader reader) {
+		System.out.println("A Card has been Swiped!");
+		stationData.setCardSwiped(true); //Inform checkout of swipe
+	}
+
+	/**
+	 * Announces that the data has been read from a card.
+	 * 
+	 * @param reader
+	 *            The reader where the event occurred.
+	 * @param data
+	 *            The data that was read. Note that this data may be corrupted.
+	 */
+	@Override
+	public void cardDataRead(CardReader reader, CardData data) {
+		if (stationData.getCardSwiped() && (data.getType() == "GiftCard") && (stationData.isWaitingForGiftCard())) {
+			stationData.setGiftCardNo(data.getNumber());
+		}
+	}
+
+    public BigDecimal payWithGiftCard(String GiftCardNumber, BigDecimal valuePurchase, GiftCardDatabase giftCardDB)
     {
-        if(valuePurchase>= this.value)
+        Map<String, GiftCardInfo> giftCardDataBase = giftCardDB.getDatabase();
+        GiftCardInfo giftCard = giftCardDataBase.get(GiftCardNumber);
+        if(valuePurchase.equals(giftCard.getBalance()) || (valuePurchase.compareTo(giftCard.getBalance()) == 1))
         {
-            valuePurchase = valuePurchase - this.value;
-            value = 0; // as all the value has been used up
+            valuePurchase = valuePurchase.subtract(giftCard.getBalance());
+            giftCard.updateBalance(giftCard.getBalance());
+             // as all the value has been used up
             return valuePurchase;
         }
 
         else
         {
-            value = value - valuePurchase;
-            valuePurchase = 0; // as we paid for the entire purchase
+            giftCard.updateBalance(valuePurchase);
+            valuePurchase.valueOf(0); // as we paid for the entire purchase
             return valuePurchase;
         }
     }
