8c063dce8b95d3b6c75489d43e94732fa1deacbb
Brodylong121 <brodylong121@gmail.com>
2022-04-09 10:17:57 -0600

    added keypad for setting partial payment

--- SCS - Software/src/org/controlSoftware/GUI/PaymentOptionGUI.java
+++ SCS - Software/src/org/controlSoftware/GUI/PaymentOptionGUI.java
@@ -2,6 +2,8 @@ package org.controlSoftware.GUI;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.math.BigDecimal;
+
 import javax.swing.*;
 import org.lsmr.selfcheckout.devices.SelfCheckoutStation;
 import org.driver.*;
@@ -41,6 +43,9 @@ public class PaymentOptionGUI {
 			break;
 		case PAYMENT_AMOUNT_PROMPT:	
 			paymentAmountScreen();
+			break;
+		case PARTIAL_PAYMENT_KEYPAD:
+			partialPaymentKeypad();
 			break;
 		default:
 			break;
@@ -297,6 +302,105 @@ public class PaymentOptionGUI {
 		frame.add(assistance);
 		assistance.setVisible(true);
 		
+	}
+	
+	//PARTIAL PAYMENT KEYPAD
+	private void partialPaymentKeypad(){
+
+		frame.setLayout(null);
+		
+		final JButton b1 = new JButton("CANCEL");
+		b1.setFont(new Font("Tahoma", Font.PLAIN, 36));
+		b1.setBounds(275, 450, 200, 100);
+		frame.getContentPane().add(b1);
+		
+		final JButton b2 = new JButton("CONFIRM");
+		b2.setFont(new Font("Tahoma", Font.PLAIN, 36));
+		b2.setBounds(525, 450, 200, 100);
+		frame.getContentPane().add(b2);
+		
+		final JLabel l1 = new JLabel("");
+		l1.setFont(new Font("Tahoma", Font.PLAIN, 40));
+		l1.setHorizontalAlignment(SwingConstants.CENTER);
+		l1.setBounds(0, 0, 1000, 100);
+		frame.getContentPane().add(l1);
+		
+		b1.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				data.changeState(StationState.PAYMENT_AMOUNT_PROMPT);
+			}
+		});
+		
+		b2.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				BigDecimal partialPaymentAmount = new BigDecimal(l1.getText());
+				BigDecimal remainingDue = data.getTotalDue().subtract(data.getTotalMoneyPaid());
+				
+				if (partialPaymentAmount.compareTo(BigDecimal.ZERO) <= 0)
+				{ //For now if user chooses to pay <= $0, default to full payment
+//					userInputScanner.close();
+					data.setTransactionPaymentAmount(remainingDue);
+					data.changeState(StationState.PAYMENT_MODE_PROMPT);
+					return; 
+				}
+				else if (partialPaymentAmount.compareTo(remainingDue) >= 0)
+				{ //If user enters in more than totalDue, default to full payment
+//					userInputScanner.close();
+					data.setTransactionPaymentAmount(remainingDue);
+					data.changeState(StationState.PAYMENT_MODE_PROMPT);
+					return; 
+				}
+//				userInput.close();
+				data.setTransactionPaymentAmount(partialPaymentAmount);
+				data.changeState(StationState.PAYMENT_MODE_PROMPT);
+				return;
+			}
+		});
+		
+		// Keypad
+		
+		final String numString = "";
+		
+		ActionListener keyList = new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JButton src = (JButton) e.getSource();
+				String val = src.getText();
+				if (val == "DEL") {
+					l1.setText(l1.getText().substring(0, l1.getText().length()-1));
+				}
+				else {
+					if (l1.getText().length() < 12) {
+						l1.setText(l1.getText() + val);
+					}
+				}
+				
+			}
+		};
+		
+		JButton[] keypad = new JButton[9];
+		for(Integer i = 0; i < keypad.length; i++) {
+			keypad[i]  = new JButton(String.valueOf((i+1) % 10));
+			keypad[i].setFont(new Font("Tahoma", Font.PLAIN, 36));
+			keypad[i].setBounds(275 + (i % 3) * 150, 100 + (int) Math.floor(i.floatValue() / 3.0) * 75, 150, 75);
+			keypad[i].addActionListener(keyList);
+			
+			frame.getContentPane().add(keypad[i]);
+		}
+		
+		JButton b3 = new JButton("DEL");
+		b3.setFont(new Font("Tahoma", Font.PLAIN, 36));
+		b3.setBounds(275, 325, 150, 75);
+		b3.addActionListener(keyList);
+		frame.getContentPane().add(b3);
+		
+		JButton b4 = new JButton("0");
+		b4.setFont(new Font("Tahoma", Font.PLAIN, 36));
+		b4.setBounds(425, 325, 150, 75);
+		b4.addActionListener(keyList);
+		frame.getContentPane().add(b4);
 	}
 	
 }
